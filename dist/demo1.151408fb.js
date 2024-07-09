30f426d0b93119b9c5d456465349cf68eba12b8e
jreynaud25 <contact@jean-rene-jean.dev>
2024-07-09 13:22:42 +0200

    mouse option

--- dist/demo1.151408fb.js
+++ dist/demo1.151408fb.js
@@ -117,7 +117,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"TNS6":[function(require,module,exports) {
+})({"../node_modules/gsap/gsap-core.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -3958,7 +3958,7 @@ var Power0 = exports.Power0 = _easeMap.Power0,
   Circ = exports.Circ = _easeMap.Circ;
 
 //export some internal methods/orojects for use in CSSPlugin so that we can externalize that file and allow custom builds that exclude it.
-},{}],"bp4Z":[function(require,module,exports) {
+},{}],"../node_modules/gsap/CSSPlugin.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -5348,7 +5348,7 @@ _gsapCore.gsap.core.getStyleSaver = _getStyleSaver;
   _gsapCore._config.units[name] = "px";
 });
 _gsapCore.gsap.registerPlugin(CSSPlugin);
-},{"./gsap-core.js":"TNS6"}],"TpQl":[function(require,module,exports) {
+},{"./gsap-core.js":"../node_modules/gsap/gsap-core.js"}],"../node_modules/gsap/index.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -5492,7 +5492,7 @@ var _CSSPlugin = require("./CSSPlugin.js");
 var gsapWithCSS = exports.default = exports.gsap = _gsapCore.gsap.registerPlugin(_CSSPlugin.CSSPlugin) || _gsapCore.gsap,
   // to protect from tree shaking
   TweenMaxWithCSS = exports.TweenMax = gsapWithCSS.core.Tween;
-},{"./gsap-core.js":"TNS6","./CSSPlugin.js":"bp4Z"}],"KkZG":[function(require,module,exports) {
+},{"./gsap-core.js":"../node_modules/gsap/gsap-core.js","./CSSPlugin.js":"../node_modules/gsap/CSSPlugin.js"}],"../node_modules/dat.gui/build/dat.gui.module.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -7967,7 +7967,7 @@ var index = {
   GUI: GUI$1
 };
 var _default = exports.default = index;
-},{}],"MnsR":[function(require,module,exports) {
+},{}],"js/demo1/menuItem.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -8271,7 +8271,7 @@ var MenuItem = /*#__PURE__*/function () {
   }]);
 }();
 var _default = exports.default = MenuItem;
-},{"gsap":"TpQl","dat.gui":"KkZG"}],"WLFM":[function(require,module,exports) {
+},{"gsap":"../node_modules/gsap/index.js","dat.gui":"../node_modules/dat.gui/build/dat.gui.module.js"}],"js/demo1/menu.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -8303,7 +8303,7 @@ var Menu = exports.default = /*#__PURE__*/_createClass(function Menu(el) {
     return _this.items.push(new _menuItem.default(item));
   });
 });
-},{"./menuItem":"MnsR"}],"MgTz":[function(require,module,exports) {
+},{"./menuItem":"js/demo1/menuItem.js"}],"js/utils.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -8321,7 +8321,7 @@ var preloadFonts = exports.preloadFonts = function preloadFonts(id) {
     });
   });
 };
-},{}],"C3Xv":[function(require,module,exports) {
+},{}],"js/demo1/index.js":[function(require,module,exports) {
 "use strict";
 
 var _menu = _interopRequireDefault(require("./menu"));
@@ -8332,4 +8332,174 @@ function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e
   document.body.classList.remove('loading');
 });
 var menu = new _menu.default(document.querySelector('nav.menu'));
-},{"./menu":"WLFM","../utils":"MgTz"}]},{},["C3Xv"], null)
+},{"./menu":"js/demo1/menu.js","../utils":"js/utils.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64440" + '/');
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      });
+
+      // Enable HMR for CSS by default.
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+  var parents = [];
+  var k, d, dep;
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+  return parents;
+}
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+  if (checkedAssets[id]) {
+    return;
+  }
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+}
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/demo1/index.js"], null)
+//# sourceMappingURL=/demo1.151408fb.js.map
---
+++ src/js/demo2/menuItem.js
@@ -0,0 +1,275 @@
+import { gsap } from "gsap";
+import * as dat from "dat.gui";
+
+class MenuItem {
+  constructor(el) {
+    this.DOM = { el: el };
+    this.DOM.textsGroupEl = this.DOM.el.querySelector("svg > g");
+    this.filterId = this.DOM.el.querySelector("svg filter").id;
+
+    this.DOM.wrapper1 = this.DOM.textsGroupEl.querySelector("#wrapper-1");
+    this.DOM.wrapper2 = this.DOM.textsGroupEl.querySelector("#wrapper-2");
+
+    this.DOM.feBlur = document.querySelector(
+      `#${this.filterId} > feGaussianBlur`
+    );
+    this.DOM.feColorMatrix = document.getElementById("colorMatrix");
+    this.primitiveValues = { stdDeviation: 15 };
+
+    this.settings = {
+      playPause: () => this.onPlayPauseClick(),
+      duration: 1.6,
+      stdDeviation: 1,
+      loop: false,
+      text1: "Be.",
+      text2: "كن",
+      colorMatrix: {
+        r: [1, 0, 0, 0, 0],
+        g: [0, 3, 0, 0, 0],
+        b: [1, 0, 1, 0, 0],
+        a: [0, 0, 0, 18, -5],
+      },
+      timeline: 0,
+      uploadSVG1: () => this.uploadSVG(this.DOM.wrapper1, "one"),
+      uploadSVG2: () => this.uploadSVG(this.DOM.wrapper2, "two"),
+      saveJSON: () => this.saveJSON(),
+      loadJSON: () => this.loadJSON(),
+    };
+
+    this.initGUI();
+    this.createTimeline();
+    this.initEvents();
+    this.addMouseMoveListener(); // Add mouse move listener
+  }
+
+  initGUI() {
+    const gui = new dat.GUI();
+    gui.add(this.settings, "playPause").name("Play/Pause");
+    gui
+      .add(this.settings, "duration", 0.1, 10, 0.1)
+      .onChange(() => this.onDurationChange());
+    gui
+      .add(this.settings, "stdDeviation", 0, 50, 0.1)
+      .onChange(() => this.onStdDeviationChange());
+    gui.add(this.settings, "loop").onChange(() => this.onLoopChange());
+    gui.add(this.settings, "text1").onChange(() => this.onText1Change());
+    gui.add(this.settings, "text2").onChange(() => this.onText2Change());
+    gui
+      .add(this.settings, "timeline", 0, 1, 0.01)
+      .onChange(() => this.onTimelineChange());
+
+    const colorMatrixFolder = gui.addFolder("Color Matrix");
+    for (let channel of ["r", "g", "b", "a"]) {
+      this.settings.colorMatrix[channel].forEach((val, i) => {
+        colorMatrixFolder
+          .add(this.settings.colorMatrix[channel], `${i}`, -10, 50, 0.1)
+          .onChange(() => this.onColorMatrixChange());
+      });
+    }
+
+    gui.add(this.settings, "uploadSVG1").name("Upload SVG 1");
+    gui.add(this.settings, "uploadSVG2").name("Upload SVG 2");
+    gui.add(this.settings, "saveJSON").name("Save Settings");
+    gui.add(this.settings, "loadJSON").name("Load Settings");
+  }
+
+  initEvents() {
+    this.DOM.textsGroupEl.style.filter = `url(#${this.filterId})`;
+  }
+
+  addMouseMoveListener() {
+    window.addEventListener("mousemove", (event) => this.onMouseMove(event));
+  }
+
+  onMouseMove(event) {
+    const timelineProgress = event.clientX / window.innerWidth;
+    const stdDeviation = 1.5 + (event.clientY / window.innerHeight) * (15 - 1.5);
+
+    this.settings.timeline = timelineProgress;
+    this.settings.stdDeviation = stdDeviation;
+
+    this.onTimelineChange();
+    this.onStdDeviationChange();
+  }
+
+  onPlayPauseClick() {
+    if (this.tl.paused()) {
+      this.tl.play();
+    } else {
+      this.tl.pause();
+    }
+  }
+
+  onTimelineChange() {
+    this.tl.pause();
+    this.tl.progress(this.settings.timeline);
+  }
+
+  onDurationChange() {
+    this.tl.duration(this.settings.duration);
+  }
+
+  onStdDeviationChange() {
+    this.DOM.feBlur.setAttribute("stdDeviation", this.settings.stdDeviation);
+    this.primitiveValues.stdDeviation = this.settings.stdDeviation;
+  }
+
+  onColorMatrixChange() {
+    const { r, g, b, a } = this.settings.colorMatrix;
+    const values = [...r, ...g, ...b, ...a].join(" ");
+    this.DOM.feColorMatrix.setAttribute("values", values);
+  }
+
+  onLoopChange() {
+    this.tl.repeat(this.settings.loop ? -1 : 0);
+    this.tl.yoyo(this.settings.loop);
+  }
+
+  onText1Change() {
+    this.DOM.wrapper1.innerHTML = `<text  x="960" y="540" text-anchor="middle" dominant-baseline="middle" font-size="100" class="one">${this.settings.text1}</text>`;
+    this.updateTextElements();
+  }
+
+  onText2Change() {
+    this.DOM.wrapper2.innerHTML = `<text x="960" y="540"  text-anchor="middle" dominant-baseline="middle" font-size="100"  class="two">${this.settings.text2}</text>`;
+    this.updateTextElements();
+  }
+
+  uploadSVG(target, className) {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = "image/svg+xml";
+    input.onchange = (event) => {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(e.target.result, "image/svg+xml");
+        const svg = doc.querySelector("svg");
+        if (svg) {
+          const viewBox = svg.viewBox.baseVal;
+          const aspectRatio = viewBox.width / viewBox.height;
+
+          // Set max height to 1080 and calculate width based on aspect ratio
+          const maxHeight = 1080;
+          const maxWidth = maxHeight * aspectRatio;
+
+          // Update SVG attributes
+          svg.setAttribute("width", "100%");
+          svg.setAttribute("height", "100%");
+          svg.classList.add(className);
+
+          // Center SVG
+          svg.style.maxHeight = `${maxHeight}px`;
+          svg.style.maxWidth = `${maxWidth}px`;
+          svg.style.display = "block";
+          svg.style.margin = "0 auto";
+
+          // Clear any existing content and append the new SVG
+          target.innerHTML = "";
+          target.appendChild(svg);
+
+          this.updateTextElements();
+        }
+      };
+      reader.readAsText(file);
+    };
+    input.click();
+  }
+
+  updateTextElements() {
+    this.DOM.wrapper1 = this.DOM.textsGroupEl.querySelector("#wrapper-1");
+    this.DOM.wrapper2 = this.DOM.textsGroupEl.querySelector("#wrapper-2");
+
+    // Ensure the newly updated elements have their initial states set correctly.
+    this.DOM.wrapper1.style.opacity = 1;
+    this.DOM.wrapper2.style.opacity = 0;
+  }
+
+  saveJSON() {
+    const settings = JSON.stringify(this.settings);
+    const blob = new Blob([settings], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "settings.json";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  }
+
+  loadJSON() {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = "application/json";
+    input.onchange = (event) => {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const loadedSettings = JSON.parse(e.target.result);
+        Object.assign(this.settings, loadedSettings);
+        this.onDurationChange();
+        this.onStdDeviationChange();
+        this.onColorMatrixChange();
+        this.onLoopChange();
+        this.onText1Change();
+        this.onText2Change();
+      };
+      reader.readAsText(file);
+    };
+    input.click();
+  }
+
+  createTimeline() {
+    const { duration } = this.settings;
+
+    this.tl = gsap
+      .timeline({
+        paused: true,
+        repeat: this.settings.loop ? -1 : 0,
+        onUpdate: () => {
+          this.DOM.feBlur.setAttribute(
+            "stdDeviation",
+            this.primitiveValues.stdDeviation
+          );
+          this.settings.timeline = this.tl.progress();
+        },
+      })
+      .to(
+        this.primitiveValues,
+        {
+          duration: duration / 2,
+          ease: "none",
+          startAt: { stdDeviation: 0 },
+          stdDeviation: () => this.settings.stdDeviation * 10,
+        },
+        0
+      )
+      .to(this.primitiveValues, {
+        duration: duration /2,
+        ease: "none",
+        stdDeviation: 0,
+        })
+        .to(
+        this.DOM.wrapper1,
+        {
+        duration: duration,
+        ease: "none",
+        opacity: 0,
+        },
+        0
+        )
+        .to(
+        this.DOM.wrapper2,
+        {
+        duration: duration,
+        ease: "none",
+        opacity: 1,
+        },
+        0
+        );
+        }
+        }
+        
+        export default MenuItem;
